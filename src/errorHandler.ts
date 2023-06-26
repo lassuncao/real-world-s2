bf50cc52903f9c74d0806a24057986126c5c45e7
Mateusz <mateusz.kwasniewski@bottega.com.pl>
2023-06-26 11:54:36 +0200

    error handling

---
+++ src/errorHandler.ts
@@ -0,0 +1,14 @@
+import { ErrorRequestHandler, RequestHandler } from "express";
+import { NotFoundError } from "./NotFoundError";
+
+export const notFoundHandler: RequestHandler = (req, res, next) => {
+    throw new NotFoundError();
+};
+
+export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+    if (err instanceof NotFoundError) {
+        return res.status(404).json({ errors: err.message });
+    }
+    console.error(err);
+    res.sendStatus(500);
+};
