e9a299d38e4340a3936bc81506f4f3676dd731b9
Mateusz <mateusz.kwasniewski@bottega.com.pl>
2023-06-26 13:42:27 +0200

    repo

---
+++ src/inMemoryArticleRepository.ts
@@ -0,0 +1,20 @@
+import { Article, ArticleId, Slug } from "./article";
+
+export const inMemoryArticleRepository = () => {
+  const articles: Record<ArticleId, Article> = {};
+
+  return {
+    async create(article: Article): Promise<void> {
+      articles[article.id] = article;
+    },
+    async update(article: Article): Promise<void> {
+      articles[article.id] = article;
+    },
+    async findBySlug(slug: Slug): Promise<Article | null> {
+      const article = Object.values(articles).find(
+        (article) => article.slug === slug
+      );
+      return article ?? null;
+    },
+  };
+};
